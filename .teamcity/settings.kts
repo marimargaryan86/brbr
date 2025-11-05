98e8201eaa797758f7d232daad550423c3fd07d8
admin <admin@TeamCity>
2025-11-05 09:52:59 +0000

    runners of 'Build' build configuration were updated (TeamCity change in 'Brbr' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -36,6 +37,14 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "sleep"
+            id = "sleep"
+            scriptContent = "sleep 10"
+        }
     }
 
     triggers {
